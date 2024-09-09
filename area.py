9885c9d52321b843fbfecc8d165290d93d099cef
Aily0409 <tanyaalexia09@gmail.com>
2024-09-09 13:31:14 -0600

    New excersices

---
+++ area.py
@@ -0,0 +1,9 @@
+def square_area():
+ size = int(input ("Hi, could you please insert a number?"))
+ area_of_square = size * size
+ print (f"The area of the square is.....{area_of_square}!")
+    
+
+square_area()    
+
+    
---
+++ trianglearea.py
@@ -0,0 +1,7 @@
+def triangle_area():
+    base = int(input("Please enter a number for the base"))
+    height = int(input("Please enter a number for the height"))
+    area_of_triangle = base*height/2
+    print (f"The area of the triangle is...{area_of_triangle}!")
+
+triangle_area()
