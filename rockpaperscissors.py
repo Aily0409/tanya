377953c5850b037c50c34b4d3a11b5b5cae199a1
Aily0409 <tanyaalexia09@gmail.com>
2024-09-13 09:07:09 -0600

    Rock paper scissors but with stars

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,26 +2,30 @@ import random
 
 options = ["rock", "paper", "scissors"]
 
+def star(text):
+    a = "*"* 10
+    b = "*" * 5
+    print (f"{a}{text}{b}")
 
 while True:
     user_ch = input("Choose rock, paper, scissors: ")
     computer_ch = random.choice(options)
-    print(computer_ch)
+    star(computer_ch)
 
     if user_ch == "quit":
         break
 
     if user_ch not in options:
-        print("not allow")
+        star("not allow")
         continue
 
     if user_ch == computer_ch:
-        print("tie")
+        star("tie")
    
     elif (user_ch == "rock" and computer_ch == "scissors") or (user_ch == "paper" and computer_ch == "rock") or (user_ch == "scissors" and computer_ch == "paper"):
-        print("Won")
+        star("Won")
 
     else:
-        print("lose")
+        star("lose")
  
     
