03edef2e54b038370434eb3b6c15d1c793ed4bd2
Aily0409 <tanyaalexia09@gmail.com>
2024-09-17 14:58:17 -0600

    A remake

--- mathfunctions.py
+++ mathfunctions.py
@@ -1,24 +1,22 @@
 import math
-factorial_numbers= (6,11,15,22)
 
-for number in factorial_numbers:
-    done_factorial= 1
-    for i in range (1, number +1):
-        done_factorial*= i
-    print (done_factorial)  
-    if done_factorial == math.factorial(number):
-        print("Verified factorial")
-        
-def Remainder(divided, divisor):
-    while divided >= divisor:
-        divided-= divisor
-    return divided
+def factorial(n):
+    result = 1
+    for index in range (2, n+1):
+        result *= index
+    print(result)
 
-division_remainder= [(3, 2), (21, 4), (131, 19), (81, 9)]
-
-for pair in division_remainder:
-    divided, divisor= pair
-    remainder_answer= Remainder(divided, divisor)
-    print("Remainder of", divided,"/", divisor, "is" ,remainder_answer)    
+factorial(6)
+factorial(11)
+factorial(15)
+factorial(22)
+    
+def remainder(a,b):
+    print(a-(b*(math.floor(a/b))))
+    
+remainder(3,2)
+remainder(21,4)
+remainder(131,19)
+remainder(81,9)
+    
          
-
