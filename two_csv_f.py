e6c3173ff19436d873c23d49b2658806b68f265d
Aily0409 <tanyaalexia09@gmail.com>
2024-11-05 15:07:29 -0600

    A lot of work

---
+++ two_csv_f.py
@@ -0,0 +1,67 @@
+import csv
+from datetime import datetime
+def read_products (filename):
+    products={}
+    with open(filename, mode="r", newline="") as csv_file:
+        reader= csv.reader(csv_file)
+        for row in reader:
+            key= row[0]
+            prod_desc=row[1]
+            prod_price = row[2]
+            products[key]=[prod_desc,prod_price]
+            
+        return products
+
+def read_request(filename):
+    product_ids = []
+    quantities = []
+    with open(filename, "rt") as csv_request:
+        reader = csv.reader(csv_request)
+        for row in reader:
+            product_ids.append(row[0])
+            quantities.append(row[1])
+        return product_ids,quantities
+    
+def main():
+    products = read_products("products.csv")
+    product_ids = read_request("request.csv")[0]
+    quantities = read_request("request.csv")[1]
+    print()
+    print("Super Convipython Store")
+    print()
+    
+    
+    subtotal = 0
+    total_items = 0
+    for i in range (len(product_ids)):
+        product = products[product_ids[i]]
+        name = product[0]
+        price = float(product[1])
+        quantity = float(quantities[i])
+        print(f"{name}:{quantity} @ {price}")
+        subtotal= subtotal+(quantity*price) 
+        total_items += quantity       
+        
+    print( )
+    print(f"Subtotal: ${subtotal:.2f}")
+    print(f"Total items: {int(total_items)}")
+    print(f"Tax: ${subtotal * 0.06:.2f}")
+    print(f"Total: ${subtotal * 1.06:.2f}")
+    
+    
+    print()
+    payment = float(input("Payment: $"))
+    change= payment - (subtotal * 1.06)
+    if change>0:
+        print()
+        print(f"Your change is: ${change:.2f}")
+        print()
+        print("Thanks for your purchase!")
+        current_data = datetime.now()
+        print(f"{current_data:%a %b %d %Y %H %M %S}")
+    else:
+        print("Not enough money")
+    
+
+    
+main()
